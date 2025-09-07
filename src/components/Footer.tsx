5a5c2c2aa150562b1420b789b045bdc4d9d29650
Quoc Nguyen <nhquoc99@gmail.com>
2025-09-07 07:49:17 +0700

    done home

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,60 @@
 import React from "react";
 
+import taiwanLogoOrange from "../assets/taiwanLogoOrange.png";
+
 const Footer: React.FC = () => {
   return (
-    <footer className="site-footer">
-      <div className="container footer-inner">
-        <p>© {new Date().getFullYear()} Taiwan Portal. All rights reserved.</p>
+    <footer
+      className="footer"
+      role="contentinfo"
+      aria-label="Thông tin liên hệ"
+    >
+      <div className="footer__container container">
+        {/* Logo bên trái */}
+        <div className="footer__brand">
+          <img
+            src={taiwanLogoOrange}
+            alt="TAIWAN • Waves of Wonder"
+            width={200}
+            height={40}
+            loading="lazy"
+            decoding="async"
+          />
+        </div>
+
+        {/* Nội dung liên hệ bên phải */}
+        <div className="footer__content">
+          <h3 className="footer__title">Liên Hệ Chúng Tôi:</h3>
+          <ul className="footer__list">
+            <li className="footer__row">
+              <strong>
+                Phòng Du Lịch – Văn Phòng Kinh Tế và Văn Hoá Đài Bắc tại Thành
+                Phố Hồ Chí Minh
+              </strong>
+            </li>
+            <li className="footer__row">
+              <span className="footer__label">Add:</span>
+              <span>
+                336 Nguyễn Trị Phương, Phường Vườn Lài, TP. Hồ Chí Minh, Việt
+                Nam.
+              </span>
+            </li>
+            <li className="footer__row">
+              <span className="footer__label">Email:</span>
+              <a href="mailto:follow@taiwan.net.vn" className="footer__link">
+                follow@taiwan.net.vn
+              </a>
+            </li>
+            <li className="footer__row">
+              <span className="footer__label">Tel:</span>
+              <span>+84-28-3834-9160 #3106 / #3107 / #3108</span>
+            </li>
+            <li className="footer__row">
+              <span className="footer__label">Office Hours:</span>
+              <span> Thứ Hai ~ Thứ Sáu — 09:00~18:00 (Trừ các ngày lễ)</span>
+            </li>
+          </ul>
+        </div>
       </div>
     </footer>
   );
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "../styles/landing.css";
 import lcpIcon from "../assets/lcpIcon.png";
 import giaiNhat from "../assets/giaiNhat.png";
@@ -9,7 +9,6 @@ import starlux from "../assets/STARLUX.png";
 import chinaAir from "../assets/CHINA.png";
 import evaAir from "../assets/EVA.png";
 
-/* team images (thêm file image vào src/assets và đổi tên import cho đúng) */
 import teamCFO from "../assets/CFO.png";
 import teamGAM from "../assets/GAM.png";
 import teamTSW from "../assets/TSW.png";
@@ -28,9 +27,23 @@ import slide2 from "../assets/TTA_PIC_SHARE.jpg";
 import slide3 from "../assets/TTA_PIC_DISCOVER.jpg";
 import slide4 from "../assets/TTA_PIC_ENJOY.jpg";
 
+import vid1 from "../assets/videos/1023_Engage_60_B.mp4";
+import promoBg from "../assets/PROMO VDO.png";
+import playBtn from "../assets/Layer 5.png";
+
 SwiperCore.use([Navigation, Autoplay]);
 
 const Landing: React.FC = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const handlePlay = () => {
+    setIsPlaying(true);
+    if (videoRef.current) {
+      videoRef.current.play();
+    }
+  };
+
   return (
     <main>
       <section className="landing-banner">
@@ -56,6 +69,7 @@ const Landing: React.FC = () => {
           </div>
         </div>
       </section>
+
       <section className="landing-prizes">
         <div className="landing-prizes__container container">
           <div className="landing-prizes__top-row">
@@ -77,6 +91,7 @@ const Landing: React.FC = () => {
           />
         </div>
       </section>
+
       <section className="landing-vote">
         <div className="container landing-vote__container">
           <button className="vote-cta">VOTE NGAY!</button>
@@ -93,7 +108,7 @@ const Landing: React.FC = () => {
         </div>
       </section>
 
-      {/* slider section */}
+      {/* slider section (images) */}
       <section className="landing-swiper">
         <div className="container landing-swiper__container">
           <div className="landing-swiper__stage">
@@ -252,136 +267,48 @@ const Landing: React.FC = () => {
                 role="region"
                 aria-label="Điều kiện và điều khoản"
               >
-                <h3 className="predict-terms__title">ĐIỀU KIỆN &amp; ĐIỀU KHOẢN</h3>
+                <h3 className="predict-terms__title">
+                  ĐIỀU KIỆN &amp; ĐIỀU KHOẢN
+                </h3>
 
                 <div className="predict-terms__inner">
                   {/* Nội dung dài sẽ nằm bên trong khung scroll 200px */}
                   <p>
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit
-                    inventore explicabo voluptas temporibus rerum, aut, dolor aperiam
-                    deserunt suscipit illum vel fuga repellendus quos minima! A
-                    doloremque, quaerat iure, deleniti voluptatum consectetur velit
-                    eveniet consequatur delectus sequi blanditiis expedita modi id
-                    molestias culpa sint! Enim, nisi minima, tenetur reiciendis
-                    dolores voluptatem omnis atque odit totam accusantium, debitis ea
-                    neque nihil porro a consequuntur recusandae? Doloremque quo
-                    corrupti accusantium ut ducimus molestias illo laboriosam
-                    ratione? Quibusdam facilis molestias impedit culpa error deserunt
-                    recusandae nihil veniam? Numquam qui atque vero id vitae. Officiis
-                    vero nemo placeat tempora culpa eaque delectus numquam ut esse cum
-                    debitis illo quaerat voluptate doloribus aliquid quos eum, hic
-                    architecto! Similique, architecto. Ullam nostrum, quia sit fugit
-                    distinctio suscipit iste reiciendis aspernatur repellendus.
-                    Reprehenderit voluptatibus rem inventore repudiandae quam numquam
-                    libero, quibusdam modi labore fugit, ut hic blanditiis esse, sunt
-                    deserunt? Porro, quos, cum deleniti, laudantium molestiae nam
-                    ullam provident sequi error iusto fuga. Recusandae unde reiciendis
-                    mollitia repudiandae nihil minus quis veniam inventore praesentium,
-                    accusamus similique totam, corrupti incidunt possimus. Accusamus at
-                    accusantium magnam eligendi iste quidem. Illum, nobis assumenda
-                    excepturi incidunt natus eius deleniti harum cumque sequi,
-                    repudiandae veniam, porro numquam dolore sunt laboriosam eum
-                    architecto ipsa accusamus doloribus illo temporibus doloremque quis
-                    molestiae. Illo, distinctio laboriosam? Aut, autem enim placeat
-                    quidem doloremque reprehenderit facilis nihil hic nisi ex? Quasi
-                    quaerat, voluptatibus aut officia provident sit veritatis quis
-                    consequuntur tempore expedita, id, cumque tempora laudantium
-                    suscipit voluptate neque fugiat temporibus adipisci unde! Nemo, ad
-                    vel! Laudantium eos aliquam labore eaque, ad beatae sed eum, est
-                    dolore deleniti voluptatibus asperiores! Obcaecati eaque sequi
-                    ratione velit harum fugit adipisci error officiis odit nam
-                    reprehenderit quo sit voluptate fugiat ipsa fuga distinctio
-                    laudantium, aspernatur natus itaque excepturi. Consequuntur, vero,
-                    excepturi, fugit quod nulla voluptatem quisquam error temporibus
-                    ipsa ex obcaecati. Fugiat iusto consectetur impedit modi tempore
-                    nisi et doloremque commodi esse aliquam distinctio a dolore
-                    similique, nam odio facilis mollitia totam officia vitae architecto
-                    perspiciatis porro aliquid expedita animi. Magnam animi soluta
-                    expedita earum voluptatum! Architecto, alias quod dicta facere
-                    natus modi explicabo veniam earum hic, placeat debitis odio
-                    necessitatibus doloribus quam magni facilis expedita est
-                    consequatur. Soluta, corporis omnis. In dolore quo perspiciatis
-                    sapiente, ducimus consequuntur libero culpa eaque! Vero, aliquam
-                    placeat. Ipsa architecto magnam beatae animi nemo culpa libero
-                    commodi, recusandae accusamus ea unde sint. Distinctio vero
-                    temporibus mollitia blanditiis, laboriosam, ratione sequi dolore
-                    aliquid doloremque quod, amet voluptate illo reiciendis libero
-                    similique ipsa sint laborum natus illum. Laborum minus maxime ab
-                    laboriosam molestiae veritatis facilis illo id explicabo ex maiores
-                    asperiores velit earum libero saepe repudiandae reiciendis iusto,
-                    provident voluptas mollitia similique autem exercitationem?
-                    Temporibus debitis quia veniam blanditiis velit alias nesciunt
-                    beatae odio placeat tempore quos itaque sequi laboriosam culpa
-                    dolorum illum laborum vero officia maiores error magni, mollitia,
-                    tempora a voluptatem. Fugiat provident nobis porro necessitatibus vel,
-                    perspiciatis at voluptatibus? Obcaecati a minus facere, omnis
-                    corporis similique vel iste enim soluta. Dolorum, consectetur
-                    commodi error excepturi expedita vero quis necessitatibus mollitia
-                    esse, laborum, sunt voluptatum! Aut ullam quisquam itaque magnam
-                    assumenda corrupti iure! Sint officia, eum a consequatur, aut
-                    cupiditate, quas quasi unde voluptatem iusto facilis cum. Fugit
-                    inventore minus beatae nulla! Cumque reprehenderit deserunt
-                    veritatis quisquam eum, repudiandae dignissimos exercitationem
-                    voluptates vero minus accusantium corporis eius nostrum deleniti
-                    quaerat porro expedita necessitatibus enim? Fugit unde nemo
-                    maxime. Ex ipsa nulla soluta distinctio eligendi at quo provident
-                    dolores. Dolore fugiat nemo ipsa dolor facere! Tenetur dignissimos
-                    iusto alias nihil ratione, quasi, provident odio adipisci libero
-                    aliquam debitis nam et consectetur dolore exercitationem doloremque
-                    enim sunt non dolores inventore quos. Atque, consequuntur asperiores
-                    eum aperiam distinctio quasi illum est voluptas cumque provident
-                    corporis, nisi perferendis sapiente qui eveniet, accusantium porro
-                    nulla vitae modi! Quisquam alias quibusdam tenetur corporis
-                    reprehenderit sapiente, quam accusamus aut dolorem dolores ratione
-                    voluptates. Nemo, tempora aliquid! Ducimus, dolores doloribus amet
-                    alias praesentium perspiciatis, aliquam nihil pariatur veritatis
-                    vero eveniet dolor id animi necessitatibus. Repudiandae adipisci
-                    beatae corporis mollitia, numquam voluptas, debitis, inventore illo
-                    nihil temporibus suscipit sapiente. Fugiat totam accusamus earum
-                    vitae hic ad velit dolores dolore magnam aspernatur quia,
-                    exercitationem molestias in illo, minima cumque at recusandae
-                    porro expedita quisquam officia sit impedit optio corrupti? Alias,
-                    at quis quam quibusdam placeat autem qui adipisci est, necessitatibus
-                    vel doloribus, sequi nam ipsa. Autem aliquid magnam, sint harum porro
-                    quidem minima dicta, qui ex mollitia repudiandae voluptas nesciunt,
-                    nobis atque saepe enim! In veritatis quasi incidunt voluptate
-                    asperiores laboriosam, aperiam reiciendis praesentium nesciunt
-                    vero ipsa. Est eius vitae explicabo facilis fuga quaerat ut impedit
-                    quibusdam. Nesciunt impedit, quod quisquam rem ipsum dicta corrupti
-                    blanditiis suscipit atque amet doloremque fuga, distinctio
-                    temporibus dolor magnam ullam nisi sint quis eos doloribus! Hic sunt
-                    mollitia nesciunt saepe error, soluta, perferendis recusandae,
-                    nostrum autem eius quas! Ea, sunt aperiam! Quam consequatur sint
-                    molestiae beatae qui exercitationem corrupti reprehenderit magnam
-                    consequuntur dolores, sed, vitae optio at assumenda eveniet
-                    sunt, illo mollitia illum. Distinctio amet quae fugit fugiat
-                    exercitationem voluptatem quas aspernatur reiciendis nemo hic. Quasi
-                    nisi libero hic sint voluptatum sed eligendi itaque at nesciunt
-                    quam vero omnis vitae tempore, deserunt sunt laborum nihil ex
-                    molestiae porro. Delectus accusamus sapiente asperiores eum sequi
-                    quam aliquam. Iure sed et dolor fugiat, animi totam nisi doloremque.
-                    Nemo perspiciatis fuga repellat quibusdam quis impedit perferendis
-                    incidunt, iure molestias doloribus quia, quos illo sequi pariatur
-                    voluptates sit adipisci dolores non velit inventore! Quibusdam
-                    nesciunt ea repellat dolore id, commodi nam vel quos eius
-                    repellendus laboriosam. Quibusdam, neque quisquam. Sed reiciendis
-                    velit, asperiores molestiae modi harum magnam ipsam mollitia dicta
-                    magni aliquid? In rerum quia reiciendis! Dolor tenetur, perferendis,
-                    aliquid obcaecati optio magnam architecto tempora quisquam, iusto
-                    natus voluptas similique maxime? Laboriosam, esse possimus vero odio
-                    incidunt voluptatum ut a tempore. Mollitia atque nam similique
-                    voluptate maiores dolorum aperiam odit illo ipsa illum architecto
-                    quis, sit tempora rerum vero corrupti nihil. Voluptate quaerat modi
-                    ullam deserunt, temporibus nihil. Corrupti corporis harum deserunt
-                    suscipit quos a beatae officiis et atque, consequatur incidunt quasi
-                    maiores itaque, illo quia rem culpa, sapiente cumque delectus
-                    vitae!
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Doloremque tempora aspernatur iure aut explicabo, quod
+                    adipisci illo assumenda fugiat aliquid optio provident
+                    voluptate? Fugit, suscipit, magni iste itaque eaque et
+                    commodi eligendi inventore veniam sequi excepturi nihil
+                    molestias quas beatae maxime aperiam? Ea quia neque illo
+                    voluptatibus incidunt doloremque sapiente.
                   </p>
                   <p>...</p>
                 </div>
               </div>
             </div>
           </div>
+        </div>
+      </section>
+
+      <section className="landing-video">
+        <div className="landing-video__container container">
+          <img
+            src={promoBg}
+            alt="Promo Background"
+            className="landing-video__bg"
+          />
+          {!isPlaying && (
+            <button className="landing-video__play-btn" onClick={handlePlay}>
+              <img src={playBtn} alt="Play" />
+            </button>
+          )}
+          <video
+            ref={videoRef}
+            className="landing-video__element"
+            src={vid1}
+            controls={isPlaying}
+            style={{ display: isPlaying ? "block" : "none" }}
+          />
         </div>
       </section>
     </main>
